0e80cdc62d214b3094a2694ff4bb3d3cfe9e1eeb
Antoine Contoux <antoinectx54@hotmail.com>
2021-05-31 13:12:39 +0200

    Create Partie.java

---
+++ src/Partie.java
@@ -0,0 +1,37 @@
+
+public class Partie {
+	public final static int EN_COURS = 0;
+	public final static int FIN = 1;
+	public final static int CONFIG = 2;
+	public final static int INIT = -1;
+	
+	private static int deplacements = 0;
+	private static int etat = INIT; 
+	
+	public static void resetDeplacements() {
+		deplacements = 0;
+	}
+	
+	public static void deplacer() {
+		deplacements++;
+	}
+	
+	public static void reset() {
+		etat = INIT;
+		resetDeplacements();
+	}
+	
+	public static int getDeplacements() {
+		return deplacements;
+	}
+	
+	public static int getEtat() {
+		return etat;
+	}
+	
+	public static void setEtat(int e) {
+		if (e==EN_COURS || e==FIN || e==CONFIG || e==INIT) {
+			etat = e;
+		}
+	}
+}
