a7538d21c068292ab1e9a40c3adaea85bbd60b70
Antoine Contoux <antoinectx54@hotmail.com>
2021-05-31 12:24:55 +0200

    Create VueTexte.java

---
+++ src/VueTexte.java
@@ -0,0 +1,27 @@
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * vue mettant a jour l'affichage dans la console
+ */
+public class VueTexte implements Observer {
+	private Grille grille;
+	
+	/**
+	 * affiche le texte de la grille
+	 */
+	public void afficher() {
+		if (grille != null) {
+			System.out.println(grille);
+		}
+	}
+	
+	/**
+	 * met a jour la vue du texte de la grille
+	 */
+	public void update(Observable o, Object arg) {
+		grille = (Grille) o;
+		
+		this.afficher();
+	}
+}
