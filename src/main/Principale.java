08a3a63b4300edde3677699b7cb343a81a01395a
Antoine Contoux <antoinectx54@hotmail.com>
2021-06-01 08:45:18 +0200

    Create Principale.java

---
+++ src/main/Principale.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Principale {
+	public static void main(String[] args) {
+		//creer une grille (modele)
+		Grille grille = new Grille();
+
+		//creer des vues
+		VueGrille vueGrille = new VueGrille();
+		VueTexte vueTxt = new VueTexte();
+		VueDeplacements vueDep = new VueDeplacements();
+		//attacher les vues au modele
+		grille.addObserver(vueGrille);
+		grille.addObserver(vueTxt);
+		grille.addObserver(vueDep);
+		vueGrille.setPreferredSize(new Dimension(400,400));
+		
+		//creer les controleurs
+		ControleurConfig ctrlConfig = new ControleurConfig(c);
+		ControleurAlea ctrlAlea = new ControleurAlea(c);
+		ControleurJouer ctrlJouer = new ControleurJouer(c);
+		ControleurQuitter ctrlQuitter = new ControleurQuitter(c);
+		ControleurGrille ctrlGrille = new ControleurGrille(c);
+
+		JPanel menu = new JPanel(new GridLayout());
+		menu.add(ctrlConfig);
+		menu.add(ctrlAlea);
+		menu.add(ctrlJouer);
+		menu.add(vueDep);
+		menu.add(ctrlQuitter);
+		
+		//ranger tout dans une frame
+		JFrame frame=new JFrame();
+		
+		frame.add(vueGrille, BorderLayout.CENTER);
+		frame.add(menu, BorderLayout.WEST);
+		frame.setSize(new Dimension(600,400));
+		frame.setVisible(true);
+		
+		// init
+		grille.changerUneLampe(0, 0);
+		grille.changerUneLampe(0, 0);
+	}
+}
